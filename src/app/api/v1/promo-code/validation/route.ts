14741d954ae910c6446ae669b7ae1bacd29a76b3
Do Ngoc Son <sondo1@cqtdt.asia>
2025-04-18 13:56:37 +0700

    test 4

--- src/app/api/v1/promo-code/validation/route.ts
+++ src/app/api/v1/promo-code/validation/route.ts
@@ -4,10 +4,10 @@ import { getInfoPromoCodeSchema } from "./promo-code.dto";
 import { requestHandler } from "@/app/api/middlewares/requestHandler";
 import { successRes } from "@/app/api/core/success.response";
 
-export const POST = requestHandler(async (req: NextRequest) => {
+export async function POST (req: NextRequest) {
   const body = await req.json();
   const data = getInfoPromoCodeSchema.parse(body);
   const result = await getInfoPromocode(data);
 
   return successRes(result);
-});
+};
